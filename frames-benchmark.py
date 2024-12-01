05ef29304f02342bc9003a21760d5b9f0678db6e
Aidan Do <aidand@canva.com>
2024-12-01 18:49:32 +1100

    .

---
+++ frames-benchmark.py
@@ -0,0 +1,13 @@
+# https://huggingface.co/datasets/google/frames-benchmark?row=35
+
+prompt = "Of the four main characters on Seinfeld, which actor is the oldest?"
+
+answer = "Michael Richards"
+
+wiki_links = [
+    "https://en.wikipedia.org/wiki/Seinfeld",
+    "https://en.wikipedia.org/wiki/Jerry_Seinfeld",
+    "https://en.wikipedia.org/wiki/Jason_Alexander",
+    "https://en.wikipedia.org/wiki/Julia_Louis-Dreyfus",
+    "https://en.wikipedia.org/wiki/Michael_Richards",
+]
