9c6579526d07319671eed86e6a84ec348d548e45
Aidan Do <aidand@canva.com>
2024-12-01 20:34:05 +1100

    .

---
+++ squad_1.py
@@ -0,0 +1,20 @@
+
+# https://github.com/superlinked/VectorHub/blob/main/docs/articles/evaluation_of_RAG_retrieval_chunking_methods.md
+# Model: https://huggingface.co/BAAI/bge-m3
+# Dataset: squad
+
+# Load in model
+
+from FlagEmbedding import BGEM3FlagModel
+
+model = BGEM3FlagModel("BAAI/bge-m3", use_fp16=True)
+
+sentences_1 = ["What is BGE M3?", "Defination of BM25"]
+sentences_2 = ["BGE M3 is an embedding model supporting dense retrieval, lexical matching and multi-vector interaction.", 
+               "BM25 is a bag-of-words retrieval function that ranks a set of documents based on the query terms appearing in each document"]
+
+embeddings_1 = model.encode(sentences_1, batch_size=12, max_length=8192)["dense_vecs"]
+embeddings_2 = model.encode(sentences_2, batch_size=12, max_length=8192)["dense_vecs"]
+
+print(embeddings_1)
+print(embeddings_2)
